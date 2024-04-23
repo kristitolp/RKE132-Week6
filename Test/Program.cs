af4f8f812c43a623150599bd625156eb9d9570e5
kristitolp <kristi.tolp@tktk.ee>
2024-04-23 19:39:39 +0300

    week6 is ready

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+//string hello = "hello!";
+//hello = hello.Replace(hello[0],char.ToUpper(hello[0]));
+//Console.WriteLine(hello);
+
+//string[] colors = { "red", "green", "blue", "pink", "yellow" };
+//Random rnd = new Random ();
+//string randomColor = colors[rnd.Next(0, colors.Length)];
+//Console.WriteLine($"You should wear {randomColor} today.");
+
+
+string dontPanic = "D0n'7 p4n1c";
+dontPanic = dontPanic.Replace('0', 'o').Replace('7', 't').Replace('4', 'a').Replace('1', 'i');
+
+Console.WriteLine(dontPanic);
